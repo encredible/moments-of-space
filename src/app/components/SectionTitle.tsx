6237f31bb3009f920524d9d4203979d3768a5cfc
encredible <descartes131@gmail.com>
2025-06-08 12:45:45 +0900

    service list 문구 수정

--- src/app/components/SectionTitle.tsx
+++ src/app/components/SectionTitle.tsx
@@ -18,9 +18,9 @@ export default function SectionTitle({
 }: TextSectionProps) {
   return (
     <div className={`text-center mb-10 md:mb-12 ${className}`}>
-      <h2 className={`text-2xl md:text-3xl font-medium mb-4 md:mb-6 text-neutral-900 ${titleClassName}`}>
+      <div className={`text-5xl md:text-3xl font-extrabold mb-4 md:mb-6 text-neutral-900 ${titleClassName}`}>
         {title}
-      </h2>
+      </div>
       <p className={`text-gray-600 max-w-3xl mx-auto whitespace-pre-line ${descriptionClassName}`}>
         {description}
       </p>
