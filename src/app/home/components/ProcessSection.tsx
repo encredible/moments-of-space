a724e363b757f104ccb8bcbb505106f13dfe1228
encredible <descartes131@gmail.com>
2025-06-13 20:33:19 +0900

    Process 문구 수정

--- src/app/home/components/ProcessSection.tsx
+++ src/app/home/components/ProcessSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Image from 'next/image';
 import homeContent from "@/app/home/content.json";
 import SectionTitle from "@/app/components/SectionTitle";
@@ -19,7 +19,7 @@ const ProcessSection = () => {
           {/* 상단 이미지 섹션 */}
           <div className="relative w-full aspect-video">
             {processSteps.map((step, index) => (
-              <div 
+              <div
                 key={step.number}
                 className={`absolute inset-0 transition-opacity duration-500 ${activeStep === index ? 'opacity-100' : 'opacity-0'}`}
               >
@@ -29,7 +29,7 @@ const ProcessSection = () => {
                   fill
                   sizes="100vw"
                   className="object-cover"
-                  style={{ clipPath: "polygon(120px 0, 100% 0, 100% 100%, 0 100%, 0 120px)" }}
+                  style={{clipPath: "polygon(120px 0, 100% 0, 100% 100%, 0 100%, 0 120px)"}}
                   priority={index === 0}
                 />
               </div>
@@ -41,9 +41,9 @@ const ProcessSection = () => {
             {/* 프로세스 번호 (가로 배치) */}
             <div className="flex justify-center space-x-6 md:space-x-12 lg:space-x-16 mb-8">
               {processSteps.map((step, index) => (
-                <div 
+                <div
                   key={step.number}
-                  className={`text-3xl md:text-4xl font-bold cursor-pointer transition-colors duration-300 ${activeStep === index ? 'text-neutral-900' : 'text-neutral-300'}`}
+                  className={`text-3xl md:text-4xl font-extrabold cursor-pointer transition-colors duration-300 ${activeStep === index ? 'text-neutral-900' : 'text-neutral-300'}`}
                   onMouseEnter={() => setActiveStep(index)}
                 >
                   {step.number}
@@ -54,12 +54,18 @@ const ProcessSection = () => {
             {/* 활성화된 단계의 설명만 표시 */}
             <div className="max-w-3xl mx-auto px-4">
               {processSteps.map((step, index) => (
-                <div 
+                <div
                   key={step.number}
                   className={`transition-opacity duration-500 ${activeStep === index ? 'opacity-100' : 'opacity-0 hidden'}`}
                 >
-                  <p className="text-center text-sm md:text-base">
+                  <p className="text-sm md:text-base pb-2 font-bold">
+                    {step.title}
+                  </p>
+                  <p className="text-sm md:text-base pb-4">
                     {step.description}
+                  </p>
+                  <p className="text-sm md:text-base font-extrabold">
+                    {step.additional}
                   </p>
                 </div>
               ))}
