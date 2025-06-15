cbde9a4366c4eec4cea7049428b40ea4ca9a46e6
encredible <descartes131@gmail.com>
2025-06-15 22:12:30 +0900

    분석 페이지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/app/components/layout/Header";
 import Footer from "@/app/components/layout/Footer";
 import MarqueeText from "@/app/components/layout/MarqueeText";
 import NavigationBar from "@/app/components/layout/NavigationBar";
-import footerContent from '@/app/components/layout/footer_content.json';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({
   children,
@@ -13,6 +13,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className="font-korean antialiased min-h-screen">
+        <Analytics />
         <MarqueeText className="sticky top-0 z-50" />
         <NavigationBar /> {/* Add NavigationBar here */}
         <Header />
