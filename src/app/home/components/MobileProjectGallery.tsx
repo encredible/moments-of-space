5329bf1646e6303abc126e150fad7c9827ffd530
encredible <descartes131@gmail.com>
2025-06-16 22:20:06 +0900

    모바일에서 갭만 수정

--- src/app/home/components/MobileProjectGallery.tsx
+++ src/app/home/components/MobileProjectGallery.tsx
@@ -94,7 +94,7 @@ const MobileProjectGallery = ({
       {/* 갤러리 스크롤 컨테이너 */}
       <div 
         ref={scrollContainerRef}
-        className="relative overflow-x-auto hide-scrollbar flex gap-12 pl-4 pr-4 pb-4"
+        className="relative overflow-x-auto hide-scrollbar flex gap-2"
         style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
       >
         {/* 이미지 목록 */}
